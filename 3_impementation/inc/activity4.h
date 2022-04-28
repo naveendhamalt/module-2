78ada4fe491b83d1b74a15c8a3f4884f3cc77ece
naveendhamalt <101941045+naveendhamalt@users.noreply.github.com>
2022-04-28 15:12:03 +0530

    Update activity4.h

--- 3_impementation/inc/activity4.h
+++ 3_impementation/inc/activity4.h
@@ -1,1 +1,24 @@
+#ifndef __ACTIVITY_4_H__
+#define __ACTIVITY_4_H__
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+#include <avr/io.h>
 
+/**
+ * @brief initializes registers
+ *
+ */
+void USARTInit(uint16_t ubrr_value);
+
+/**
+ * @brief transmits character
+ *
+ */
+void USARTWriteChar(char data);
+
+/**
+ * @brief Passes a string character by character to USARTWriteChar(char)
+ *
+ */
+void USARTWritestring(char data[]);
+
+#endif /* __ACTIVITY_4_H__ */
