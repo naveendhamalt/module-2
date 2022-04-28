1cd44997036af0441acdd7f28e3963424170719c
naveendhamalt <101941045+naveendhamalt@users.noreply.github.com>
2022-04-28 15:11:22 +0530

    Update activity3.h

--- 3_impementation/inc/activity3.h
+++ 3_impementation/inc/activity3.h
@@ -1,1 +1,13 @@
+#ifndef __ACTIVITY_3_H__
+#define __ACTIVITY_3_H__
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+#include <avr/io.h>
 
+/**
+ * @brief uses value from adc to make pwm signal of corresponding duty cycle
+ *
+ * 
+ */
+void pwm_init();
+
+#endif /* __ACTIVITY_3_H__ */
